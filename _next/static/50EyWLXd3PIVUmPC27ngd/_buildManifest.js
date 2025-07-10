ce7bdc6303451e3ba2a6c1c35dadf90aa192a26f
umair <eeumairali@gmail.com>
2025-07-11 03:51:23 +0500

    Deploy Next.js site to GitHub Pages

---
+++ _next/static/50EyWLXd3PIVUmPC27ngd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/766-9154d8c528a893b6.js","static/chunks/pages/index-9136d3aaf2b4c80b.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
